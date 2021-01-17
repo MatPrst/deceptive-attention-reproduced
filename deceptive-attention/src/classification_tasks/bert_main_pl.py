faf8217345ff2d3bfed4179f5fe596e225154216
Ard Snijders <ardsnijders@gmail.com>
2021-01-17 17:08:53 +0100

    added attention mask optional arg in bert fwd call added function to compute R penalty added function to generate matrix masks M for info flow restriction added dropout

--- deceptive-attention/src/classification_tasks/bert_main_pl.py
+++ deceptive-attention/src/classification_tasks/bert_main_pl.py
@@ -15,10 +15,8 @@ from torch.cuda import device_count
 import sys
 import time
 
-
-
 # Local dependencies
-from bert_attention import BertSelfAttention_Altered
+from bert_attention import BertSelfAttention_Altered, BertModel_Altered, BertEncoder_Altered
 from bert_util_pl import GenericDataModule
 
 import os
@@ -26,13 +24,14 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 class BERTModel(LightningModule):
 
-    def __init__(self, penalize, lambeda, penalty_fn):
+    def __init__(self, dropout, penalize, lambeda, penalty_fn):
         super().__init__()
         """
         Args:
             penalize: flag to toggle attention manipulation and information flow restriction
             tokenizer: transformers object used to convert raw text to tensors
         """
+        self.dropout = dropout
         self.penalize = penalize
         self.lambeda = lambeda
         self.penalty_fn = penalty_fn
@@ -41,22 +40,25 @@ class BERTModel(LightningModule):
         if self.penalize:
             # if we're penalizing the model's attending to impermissible tokens, we want to overwrite the original
             # self-attention class in the transformers library with a local class which has been adapted to ensure
-            # the restriction of information flow between permissible and impermissible tokens
+            # # the restriction of information flow between permissible and impermissible tokens
             transformers.models.bert.modeling_bert.BertSelfAttention = BertSelfAttention_Altered
 
         # load pretrained, uncased model
-        self.encoder = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
+        self.encoder = BertForSequenceClassification.from_pretrained('bert-base-uncased',
+                                                                     num_labels=2,
+                                                                     hidden_dropout_prob=self.dropout)
 
     def configure_optimizers(self):
         "This method handles optimization of params for PyTorch lightning"
         return Adam(self.parameters(), lr=config.lr)
 
-    def forward(self, x, attention_mask, labels, head_mask=None):
-        "This method defines how the data is passed through the net"
-        if self.penalize:
-            output = self.encoder(x, labels=labels, attention_mask=attention_mask, head_mask=head_mask, output_attentions=True)
-        else:
-            output = self.encoder(x, labels=labels, attention_mask=attention_mask, output_attentions=True)
+    def forward(self, x, attention_mask, labels, mask_matrices):
+        "This method defines how the data is passed through the net."
+        output = self.encoder(x,
+                              labels=labels,
+                              attention_mask=attention_mask,
+                              head_mask=mask_matrices,
+                              output_attentions=True)
 
         return output
 
@@ -70,35 +72,24 @@ class BERTModel(LightningModule):
         sentences = batch['sentences']
         attention_masks = batch['attention_masks']
         mask_vectors = batch['mask_vectors']
-
-        print('first sentence ids: ')
-        print(sentences[0].shape)
-        print(sentences[0])
+        mask_matrices = batch['mask_matrices'].unsqueeze(1) # add necessary implicit 1st dim
 
         # flag to toggle manipulation of attention maps
         if self.penalize:
 
-            # TODO: Generate self attention mask for info flow restriction
-            # self_attention_masks = self.generate_mask(tokenized_sents["input_ids"],
-            #                                      tokenized_impermissible["input_ids"])
-
             # Feed sentences through network
-            outputs = self(x=sentences, attention_mask=attention_masks, labels=labels)
+            outputs = self(x=sentences, attention_mask=attention_masks, mask_matrices=None, labels=labels)
+
             # Compute loss w.r.t. predictions and labels
             loss = outputs.loss
 
-            # TODO compute R component to add to loss
-            # loss = loss + <R>
-            R = self.compute_R(outputs, mask_vectors, self.lambeda, self.penalty_fn).sum()
-
-            print(R)
-
-
+            # Compute R component and add to loss
+            R = self.compute_R(outputs, mask_vectors, self.lambeda, self.penalty_fn).mean()
             loss += R
 
-
         # Log loss to Tensorboard
-        self.log('training_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log('training_loss', loss, on_step=True, prog_bar=True, logger=True)
+        self.log('train_penalty_R', R, on_step=True, prog_bar=True, logger=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -111,30 +102,27 @@ class BERTModel(LightningModule):
         sentences = batch['sentences']
         mask_vectors = batch['mask_vectors']
         attention_masks = batch['attention_masks']
+        mask_matrices = batch['mask_matrices'].unsqueeze(1)
 
         # flag to toggle manipulation of attention maps
         if self.penalize:
 
-            # TODO: Generate self attention mask for info flow restriction
-            # self_attention_masks = self.generate_mask(tokenized_sents["input_ids"],
-            #                                      tokenized_impermissible["input_ids"])
-
             # Feed sentences through network
-            outputs = self(x=sentences, attention_mask=attention_masks, labels=labels)
+            outputs = self(x=sentences, attention_mask=attention_masks, mask_matrices=None, labels=labels)
+
             # Compute loss w.r.t. predictions and labels
             loss = outputs.loss
 
-            # TODO compute R component to add to loss
-            # loss = loss + <R>
+            # Compute loss w.r.t. predictions and labels
+            loss = outputs.loss
 
-            # # extract self-attention maps of shape [batch_size, num_layers, num_heads, sequence_length, sequence_length]
-            # attention_matrices = outputs.attentions
-            #
-            # # convert attention matrices to vectors
-            # attention_vectors = self.convert2vectors(attention_matrices)
+            # Compute R component and add to loss
+            R = self.compute_R(outputs, mask_vectors, self.lambeda, self.penalty_fn).mean()
+            loss += R
 
         # Log loss to Tensorboard
-        self.log('validation_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log('dev_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log('dev_penalty_R', R, on_step=True, prog_bar=True, logger=True)
 
         return None
 
@@ -188,8 +176,10 @@ class BERTModel(LightningModule):
         # take max of n x n attention maps along row-dimension
         matrices, _ = torch.max(matrices, dim=3, out=None)
 
-        # or, take diagonal of attention maps?
-        # matrices = torch.diagonal(matrices, dim1=3, dim2=4)
+        # renormalize to 1?
+        m = nn.Softmax(dim=3)
+        matrices[matrices == 0.0] = -9999
+        matrices = m(matrices)
 
         return matrices
 
@@ -201,48 +191,32 @@ class BERTModel(LightningModule):
         # extract self-attention maps of shape [batch_size, num_layers, num_heads, sequence_length, sequence_length]
         attention_matrices = outputs.attentions
 
-        print('attention matrix for first sample, first layer, first head')
-        print(attention_matrices[0][0][0].shape)
-        print(attention_matrices[0][0][0])
-
-        #TODO: attention matrices hebben ook non-negative waarden op de allerbuitenste indices
-
         # convert attention matrices to vectors
         attention_vectors = self.convert2vectors(attention_matrices)
 
         sequence_length = attention_vectors.shape[-1]
         if penalty_fn == 'mean':
 
-            print('attention vector shape for batch of size 8:')
-            print(attention_vectors.shape)
-            print('attention vector for first sample, first layer, first head')
-            print(attention_vectors[0][0][0])
-            print('summing over sequence-length dimension: ')
-            print(torch.sum(attention_vectors[0][0][0]))
-
-            #TODO: explore possibility of head_mask argument
-
-            sys.exit()
-
-            # reshape batch_size * 12 x 12 (12 layers, 12 heads) x seq_length to batch_size, 144, seq_length
+            # reshape tensor of shape batch_size * 12 x 12 x seq_length to
+            # shape batch_size, 144, seq_length
             attention_vectors = attention_vectors.reshape(config.batch_size, 12**2, sequence_length)
 
             # add implicit dimension to mask_vectors such that it becomes a rank-4 tensor
             mask_vectors = mask_vectors.unsqueeze(1)
 
-            # compute impermissible attention tensor of shape (batch_size, 12**2, seq_length)
+            # compute impermissible attention tensor of shape (batch_size, ||Heads H||, seq_length)
             impermissible_attention = attention_vectors * mask_vectors
 
             # Sum over last dim to get impermissible attention per head
             impermissible_attention = torch.sum(impermissible_attention, dim=2)
 
-            # Compute the complement or permissible attention
+            # Compute the complement of impermissible attention, or permissible attention
             permissible_attention = 1 - impermissible_attention
 
             # log permissible attention per head
             log_permissible_attention = torch.log(permissible_attention)
 
-            # Compute R value using 'mean' method
+            # Compute R value using 'mean' method by summing over H dimension and dividing by 144
             R = (-lambeda/144)*(torch.sum(log_permissible_attention, dim=1))
 
             return R
@@ -258,19 +232,16 @@ def main(args):
     print('no of devices found: {}'.format(device_count()))
 
     # Logic to define model with specified R calculation, specified self attn mask
-    model = BERTModel(penalize=config.penalize, lambeda=config.lambeda, penalty_fn=config.penalty_fn)
+    model = BERTModel(dropout=config.dropout, penalize=config.penalize, lambeda=config.lambeda, penalty_fn=config.penalty_fn)
 
-    # Crude logic to freeze all the BERT parameters
-    # for param in model.encoder.parameters():
-    #     param.requires_grad = False
 
     # Main Lightning code
     tb_logger = pl_loggers.TensorBoardLogger('logs/')
     trainer = Trainer(gpus=config.gpus, logger=tb_logger)
     dm = GenericDataModule(task=config.task,
-                                   anonymization=config.anon,
-                                   max_length=config.max_length,
-                                   batch_size=config.batch_size)
+                           anonymization=config.anon,
+                           max_length=config.max_length,
+                           batch_size=config.batch_size)
     # dm.setup()
     trainer.fit(model, dm)
 
@@ -285,9 +256,10 @@ if __name__ == '__main__':
                         help='no. of sentences sampled per pass')
     parser.add_argument('--lr', default=5e-5, type=float,
                         help='learning rate')
-
+    parser.add_argument('--dropout', default=0.3, type=float,
+                        help='hidden layer dropout prob')
     parser.add_argument('--max_length', default=180, type=int,
-                        help='max no of tokens for tokenizer')
+                        help='max no of tokens for tokenizer (default is enough for all tasks')
 
     # Task specific args
     parser.add_argument('--task', default='occupation', type=str,
@@ -296,7 +268,7 @@ if __name__ == '__main__':
                         help='arg to toggle anonymized tokens')
     parser.add_argument('--penalize', default=True, type=bool,
                         help='flag to toggle penalisation of attn to impermissible words')
-    parser.add_argument('--lambeda', default=0.1, type=float,
+    parser.add_argument('--lambeda', default=1, type=float,
                         help='penalty coefficient')
     parser.add_argument('--penalty_fn', default='mean', type=str,
                         help='penalty fn [options: mean or max')
--- deceptive-attention/src/classification_tasks/bert_util_pl.py
+++ deceptive-attention/src/classification_tasks/bert_util_pl.py
@@ -16,13 +16,6 @@ import sys
 # class SSTDataset(Dataset):
 #     raise NotImplementedError
 
-def generate_mask(self, tokenized_sents_ids, tokenized_impermissible_ids):
-    """
-    This function generates the self-attention mask M required to ensure
-    a restriction in the information flow between permissible and impermissible tokens.
-    """
-    raise NotImplementedError
-
 def compute_m(sentence_ids, impermissible_ids):
     """
     This function computes the masking vector m, as used to compute the penalty R
@@ -38,6 +31,124 @@ def compute_m(sentence_ids, impermissible_ids):
     mask = np.asarray([1 if idx in impermissible_ids else 0 for idx in sentence_ids])
 
     return mask
+
+def compute_M(sentence_ids, impermissible_ids):
+    """
+    This function computes the masking matrix M,
+    as used to restrict the information flow between different sets of tokens,
+    for a given sentence and a set of impermissible tokens.
+
+    args:
+    sentence_ids: array of token ids of shape [1, max_seq_length]
+    impermissible_ids: array of token ids of shape [1, || impermissible tokens || ]
+    returns:
+    one-hot array of shape [1, max_seq_length]
+    """
+
+    # ------ Defining attention mask M ------
+    sequence_length = sentence_ids.shape[0]
+    mask_matrix = np.zeros((sequence_length, sequence_length))
+    # define the "sets"
+    permissible_set = set(sentence_ids) - set(impermissible_ids)
+    impermissible_set = set(impermissible_ids)
+    # set entry = 1 where token i and j belong to same sets
+    for i in range(sequence_length):
+        for j in range(sequence_length):
+            if sentence_ids[i] in permissible_set and sentence_ids[j] in permissible_set:
+                mask_matrix[i][j] = 1
+            if sentence_ids[i] in impermissible_set and sentence_ids[j] in impermissible_set:
+                mask_matrix[i][j] = 1
+
+    # ------ Dealing with CLS token ------
+    # None of the other tokens should attend to the CLS token
+    mask_matrix[:, 0] = 0
+    # CLS token should attend to all tokens (including itself)
+    mask_matrix[0,:] = 1
+
+    # ------ Dealing with padding tokens ------
+    try:
+        # None of the padding tokens should attend to each other
+        padding_idx = np.where(sentence_ids == 0)[0][0] # idx at which first padding token occurs
+        # set all columns belonging to padding token ids to 0
+        mask_matrix[:, padding_idx:] = 0
+        # set all rows belonging to padding token ids to 0
+        mask_matrix[padding_idx:, :] = 0
+    except IndexError:
+        pass
+
+    return mask_matrix
+
+class PronounDataset(Dataset):
+    """
+    This class loads the desired data split for the Pronoun Classification dataset
+    """
+    #TODO: downsample minority classes?
+
+    def __init__(self, dataset, max_length=180, anonymization=False):
+        """
+        Args:
+
+        """
+        self.path = './data/pronoun/' + dataset + '.txt'
+        self.text = read_csv(self.path, sep="\t", header=None)
+
+        self.anonymization = anonymization
+        self.impermissible = "he she her his him Ms Mr"
+
+        self.tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+        self.impermissible_ids = self.tokenizer(text=self.impermissible, return_tensors='np')['input_ids'][0][1:-1]
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.text)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        label, sentence = self.text.iloc[idx]
+
+        # convert capitalized <str> labels to binary
+        if label == 'F':
+            label = 0
+        else:
+            label = 1
+
+        # if anonymization, we remove the impermissible words from each sentence
+        splitted_sentence = sentence.split()
+        if self.anonymization:
+            sentence = ' '.join([i for i in splitted_sentence if i not in self.impermissible]) # shouldn't it be self.impermissible.split()?
+
+        # tokenize sentence and convert to sequence of ids
+        tokenized = self.tokenizer.encode_plus(
+            sentence,
+            add_special_tokens=True,
+            max_length=self.max_length,
+            padding='max_length',
+            return_attention_mask=True,
+            return_tensors='np',
+            truncation=True
+        )
+
+        sentence = tokenized['input_ids'][0]
+        attention_mask = tokenized['attention_mask'][0]
+
+        # compute vector mask m for each sentence
+        mask_vector = compute_m(sentence, self.impermissible_ids)
+
+        # compute matrix mask M for each sentence
+        mask_matrix = compute_M(sentence, self.impermissible_ids)
+
+        # convert np arrays to tensors
+        sentence = torch.from_numpy(sentence)
+        attention_mask = torch.from_numpy(attention_mask)
+        mask_vector = torch.from_numpy(mask_vector)
+        mask_matrix = torch.from_numpy(mask_matrix)
+
+        # return sample dict with sentence, associated label, and one-hot mask_vectors m
+        sample = {'sentences': sentence, 'attention_masks': attention_mask,
+                  'labels': label, 'mask_vectors': mask_vector, 'mask_matrices': mask_matrix}
+        return sample
 
 class OccupationDataset(Dataset):
     """
@@ -91,14 +202,18 @@ class OccupationDataset(Dataset):
         # compute vector mask m for each sentence
         mask_vector = compute_m(sentence, self.impermissible_ids)
 
+        # compute matrix mask M for each sentence
+        mask_matrix = compute_M(sentence, self.impermissible_ids)
+
         # convert np arrays to tensors
         sentence = torch.from_numpy(sentence)
-        mask_vector = torch.from_numpy(mask_vector)
         attention_mask = torch.from_numpy(attention_mask)
+        mask_vector = torch.from_numpy(mask_vector)
+        mask_matrix = torch.from_numpy(mask_matrix)
 
         # return sample dict with sentence, associated label, and one-hot mask_vectors m
         sample = {'sentences': sentence, 'attention_masks': attention_mask,
-                  'labels': label, 'mask_vectors': mask_vector}
+                  'labels': label, 'mask_vectors': mask_vector, 'mask_matrices': mask_matrix}
         return sample
 
 class GenericDataModule(pl.LightningDataModule):
@@ -118,6 +233,10 @@ class GenericDataModule(pl.LightningDataModule):
             self.train = OccupationDataset(dataset='train', max_length=self.max_length, anonymization=self.anonymization)
             self.val = OccupationDataset(dataset='dev', max_length=self.max_length,  anonymization=self.anonymization)
             self.test = OccupationDataset(dataset='test', max_length=self.max_length,  anonymization=self.anonymization)
+        if self.task == 'pronoun':
+            self.train = PronounDataset(dataset='train', max_length=self.max_length, anonymization=self.anonymization)
+            self.val = PronounDataset(dataset='dev', max_length=self.max_length,  anonymization=self.anonymization)
+            self.test = PronounDataset(dataset='test', max_length=self.max_length,  anonymization=self.anonymization)
 
     def train_dataloader(self):
         return DataLoader(self.train, batch_size=self.batch_size, num_workers=4)
